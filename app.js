679e0620e2990d2feed643de707484bfad165330
Konrad <konradbieniek@gmail.com>
2023-07-14 22:48:08 +0200

    add style

--- app.js
+++ app.js
@@ -11,25 +11,53 @@ const __dirname = path.dirname(__filename)
 const app = express()
 
 let itemsArr = []
+let foodItems = []
 
 const today = new Date()
 
 let day = ""
+const options = {
+	weekday: "long",
+	year: "numeric",
+	month: "long",
+	day: "numeric",
+}
 
 app.set("view engine", "ejs")
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-	day = today.toLocaleString("eng", { weekday: "long" })
+	day = today.toLocaleString("eng", options)
 
-	res.render("index", { kindOfDay: day, itemEls: itemsArr })
+	res.render("index", { listTitle: day, itemEls: itemsArr })
 })
 
 app.post("/", (req, res) => {
 	const newItem = [req.body.newItemInput]
-	itemsArr.push(newItem)
-	res.redirect('/')
+	if (req.body.list === "food") {
+		foodItems = [...foodItems, newItem]
+		res.redirect("/food")
+	} else {
+		itemsArr = [...itemsArr, newItem]
+		res.redirect("/")
+	}
 })
 
-app.listen(port, () => console.log(`server run on port::${port}`))
+app.get("/food", (req, res) => {
+	res.render("index", {
+		listTitle: "Food list",
+		itemEls: foodItems,
+	})
+})
+
+app.post("/food", (req, res) => {
+	const newFood = [req.body.newItemsInput]
+	foodItems = [...foodItems, newFood]
+	res.redirect("/food")
+})
+
+app.listen(port, () => {
+	console.log(`server run on port::${port}`)
+})
