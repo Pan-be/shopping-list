c364a8f1297064640a41973e334f60357a364d3c
panbe <konradbieniek@gmail.com>
2023-07-23 22:27:27 +0200

    connect with mongodb atlas, Create and Read from CRUD

--- app.js
+++ app.js
@@ -4,8 +4,13 @@ import https from "https"
 import path from "path"
 import { fileURLToPath } from "url"
 import mongoose from "mongoose"
+import "dotenv/config"
 
-const port = 3000
+const port = process.env.PORT
+const adminName = process.env.ADM_NAME
+const adminPassword = process.env.ADM_PASSWORD
+
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
@@ -28,15 +33,48 @@ app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
-mongoose.connect('mongodb://localhost:27017/shoppingListDB')
+mongoose.connect(`mongodb+srv://${adminName}:${adminPassword}@cluster0.axfb9st.mongodb.net/?retryWrites=true&w=majority/shoppingListDB`)
+
+
+const itemSchema = {
+	itemName: String
+}
+
+const Item = mongoose.model('Item', itemSchema
+)
+
+const item = new Item({
+	itemName: 'Welcome to your shopping list'
+})
+
+const item2 = new Item({
+	itemName: 'Hit the + button to add a new to-buy-item'
+})
+
+const item3 = new Item({
+	itemName: '<----Hit this to delete an item'
+})
+
+const defaultItems = [item, item2, item3]
+
+
 
 app.get("/", (req, res) => {
 	day = today.toLocaleString("eng", options)
 
-	res.render("index", {
-		listTitle: day,
-		itemEls: itemsArr,
-		year: year,
+	Item.find({}).then((foundItems) => {
+
+		if (foundItems.length === 0) {
+			Item.insertMany(defaultItems)
+			res.redirect('/')
+		} else {
+
+			res.render("index", {
+				listTitle: day,
+				itemEls: foundItems,
+				year: year,
+			})
+		}
 	})
 })
 
